7fc2cc170a32ed9128c775aba7bacbc29d0ce8a4
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-07-03 02:53:19 +0530

    Add files via upload

---
+++ video 54 intro to javascript/script.js
@@ -0,0 +1,15 @@
+
+alert("Hello world");
+console.log("code is running")
+console.log("code is also running")
+console.log("code is running like a wow")
+var a = prompt("enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and want to blast your computer")
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not Blasting")
+}
+console.log("Your number is " + a)
+
