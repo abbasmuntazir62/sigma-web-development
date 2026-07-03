0356237cd2d3fe7e2fddc54a256e8e21f9569c8d
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-07-03 15:55:35 +0530

    Add files via upload

---
+++ video 56/index.js
@@ -0,0 +1,38 @@
+console.log("Hey I am the best engineer")
+ 
+     let age = 1;
+    //  let grace = 2;
+    //  age += grace
+    //  console.log(age)
+    //  console.log(age + grace)
+    //   console.log(age - grace)
+    //    console.log(age * grace)
+    //     console.log(age / grace)
+
+
+if (age == 18){
+    console.log("you can drive");
+}
+else if (age == 0){
+    console.log("Are you kidding?")
+}
+else if (age == 1){
+    console.log("Are you again kidding?")
+}
+else {
+    console.log("you cannot drive");
+}
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+console.log(c);
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
