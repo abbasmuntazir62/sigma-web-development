e39b4a13bfbb5439c388702f3a60cdc270d96e67
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-07-04 00:58:59 +0530

    Add files via upload

---
+++ video 57/index.js
@@ -0,0 +1,35 @@
+console.log("Hey this is conditional statement tutorial")
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+// let obj = {
+//     name: "Harry",
+//     role: "programmer",
+//     company: "codewithHarry AI"
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log (key)
+// }
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+// while loop
+// let i = 0;
+// while (i < 6000){
+//     console.log(i)
+//     i++;
+// }
+
+// do while loop
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while( i < 6);
