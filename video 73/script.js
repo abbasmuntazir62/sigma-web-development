8f1cf9ec2aff8ff16218ada5c7116f4d97caf6f4
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-07-16 01:06:20 +0530

    Add files via upload

---
+++ video 73/script.js
@@ -0,0 +1,27 @@
+function createcard(title, cName, views, month, thumbnail, duration) {
+    //finish this function
+       let viewStr
+    if (views < 1000) {
+        viewStr = views;
+    }
+    else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
+    }
+    else {
+        viewStr = views / 1000 + "K";
+    }
+      let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${title}</h1>
+        <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+    </div> 
+</div>`
+
+        }
+
+createcard("sigma web development, codewithharry, 56K, 5, 31: 24, https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLB6VzxANxt3dHm91HW9lLfDlxvmuA")
